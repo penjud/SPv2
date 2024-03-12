7ea952544148be9369cdf46ad19a77ed832b0f1b
penjud <wicksy1770@gmail.com>
2024-03-12 17:19:24 +1000

    Commit22

--- betfair_bot/settings.py
+++ betfair_bot/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'betfair_bot.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env.str("DB_NAME"),
-        'USER': env.str("DB_USER"),
-        'PASSWORD': env.str("DB_PASSWORD"),
-        'HOST': env.str("DB_HOST"),
-        'PORT': env.str("DB_PORT", default="5432"),
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
     }
 }
 
